0109ca04a05a92fff9d611a56e51f7769c2e13e8
SankalpM <sankalpmovva2@gmail.com>
2025-12-26 12:42:14 +0100

    removing unused endpoints

--- routes/admin/tmdb.js
+++ routes/admin/tmdb.js
@@ -22,39 +22,6 @@ router.post('/import', async (req, res) => {
   }
 });
 
-//serch movies from TMDB
-
-router.get('/search', async (req, res) => {
-  const { title } = req.query;
-  if (!title) return res.status(400).json({ error: "Title is required" });
-
-  try {
-    const response = await axios.get(`${TMDB_BASE_URL}/search/movie`, {
-      params: {
-        api_key: TMDB_API_KEY,
-        query: title,
-        language: 'en-US',
-        page: 1,
-        include_adult: false
-      }
-    });
-
-    const movie = response.data.results[0];
-    if (!movie) return res.status(404).json({});
-
-    res.json({
-      TMDB_ID: movie.id,
-      Title: movie.title,
-      Poster_URL: movie.poster_path ? `https://image.tmdb.org/t/p/w500${movie.poster_path}` : '',
-      Overview: movie.overview,
-      Release_Date: movie.release_date,
-      Rating: movie.vote_average
-    });
-  } catch (err) {
-    console.error('TMDB route error:', err.message);
-    res.status(500).json({ error: err.message });
-  }
-});
 
 
 module.exports = router;
